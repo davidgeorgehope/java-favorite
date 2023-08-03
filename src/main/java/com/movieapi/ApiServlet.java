6dfef87cd621c7f550d7ce4210080a2e874e16c5
David Hope <david.hope@elastic.co>
2023-08-03 11:54:12 -0400

    new stuff

--- src/main/java/com/movieapi/ApiServlet.java
+++ src/main/java/com/movieapi/ApiServlet.java
@@ -1,15 +1,10 @@
 package com.movieapi;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-
+import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.trace.StatusCode;
 import io.opentelemetry.context.Scope;
 import org.apache.logging.log4j.LogManager;
@@ -18,17 +13,20 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
-import javax.annotation.PostConstruct;
+import redis.clients.jedis.JedisPoolConfig;
 
-import io.opentelemetry.api.GlobalOpenTelemetry;
-import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.api.trace.Tracer;
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/favorites")
 public class ApiServlet {
     private static final Logger logger = LogManager.getLogger(ApiServlet.class);
 
@@ -43,79 +41,126 @@ public class ApiServlet {
     @Value("${REDIS_PORT:6379}")
     private Integer redisPort;
 
+    String SERVICE_NAME = System.getenv("OTEL_SERVICE_NAME");
+
 
     private JedisPool r;
 
     @PostConstruct
     public void init() {
-        r = new JedisPool(redisHost, redisPort);
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxWaitMillis(3000); // Set the maximum blocked time to 3 seconds
+        poolConfig.setMaxTotal(100); // set the max total connections
+        r = new JedisPool(poolConfig, redisHost, redisPort);
     }
 
     @GetMapping
     public String helloWorld(@RequestParam(required = false) String user_id) throws InterruptedException {
-        Span span = GlobalOpenTelemetry.getTracer("YEAH").spanBuilder("helloWorld").startSpan();
+        Span span = GlobalOpenTelemetry.getTracer(SERVICE_NAME).spanBuilder("helloWorld").startSpan();
         Scope scope = span.makeCurrent();
+        String returnedString = "";
 
         if (user_id == null) {
-
             logger.info("Main request successful");
-            span.addEvent("a span event", Attributes
-                    .of(AttributeKey.longKey("someKey"), Long.valueOf(93)));
-
-            span.setStatus(StatusCode.OK);
-
-            span.end();
-
-            scope.close();
-            return "Hello World!";
+            returnedString= "Hello World!";
         } else {
-            span.addEvent("a span event", Attributes
-                    .of(AttributeKey.longKey("someKey"), Long.valueOf(93)));
-
-            span.setStatus(StatusCode.OK);
-
-            span.end();
-
-            scope.close();
-            return getUserFavorites(user_id);
+            returnedString = getUserFavorites(user_id);
         }
 
+        span.addEvent("a span event", Attributes
+                .of(AttributeKey.longKey("someKey"), Long.valueOf(93)));
+        span.setStatus(StatusCode.OK);
+        span.end();
+        scope.close();
+
+        return returnedString;
 
     }
+    public void postUserFavorites(String user_id, String movieID) {
+        Span span = GlobalOpenTelemetry.getTracer(SERVICE_NAME).spanBuilder("Redis.Post").setSpanKind(SpanKind.CLIENT).startSpan();
+        Jedis jedis = r.getResource();
+
+        try (Scope scope = span.makeCurrent()) {
+            span.setAttribute("db.system", "redis");
+            span.setAttribute("db.connection_string", redisHost);
+            span.setAttribute("db.statement", "POST user_id " + user_id +" AND movie_id "+movieID);
+
+                Long redisResponse = jedis.srem(user_id, movieID);
+                if (redisResponse == 0) {
+                    jedis.sadd(user_id, movieID);
+                }
+        } catch (Exception e) {
+            span.setStatus(StatusCode.ERROR, "Error while getting data from Redis");
+            span.recordException(e);
+        } finally {
+            jedis.close();
+            span.end();
+        }
+    }
+
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public String handlePost(@RequestParam String user_id, @RequestBody JSONObject requestBody) throws InterruptedException, Exception {
+    public String handlePost(@RequestParam String user_id, @RequestBody String requestBody) throws InterruptedException, Exception {
+        Span span = GlobalOpenTelemetry.getTracer(SERVICE_NAME).spanBuilder("handlePost").startSpan();
+        Scope scope = span.makeCurrent();
+
         handleDelay();
         logger.info("Adding or removing favorites");
-        String movieID = Integer.toString(requestBody.getInt("id"));
+
+        JSONObject json = new JSONObject(requestBody);
+        String movieID = Integer.toString(json.getInt("id"));
+
+       /// String movieID = Integer.toString(requestBody.getInt("id"));
 
         logger.info("Adding or removing favorites for user " +  user_id + ", movieID " + movieID);
 
-        Long redisResponse = r.getResource().srem(user_id, movieID);
-        if (redisResponse == 0) {
-            r.getResource().sadd(user_id, movieID);
-        }
-
+        postUserFavorites(user_id,movieID);
         String favorites = getUserFavorites(user_id);
-
         handleCanary();
 
+        span.addEvent("a span event", Attributes
+                .of(AttributeKey.longKey("someKey"), Long.valueOf(93)));
+
+        span.setStatus(StatusCode.OK);
+
+        span.end();
+
+        scope.close();
         return favorites;
     }
+    public String getUserFavorites(String user_id) {
+        Span span = GlobalOpenTelemetry.getTracer(SERVICE_NAME).spanBuilder("Redis.Get").setSpanKind(SpanKind.CLIENT).startSpan();
+        String returnedFavorites = "";
+        Jedis jedis = r.getResource();
 
-    private String getUserFavorites(String user_id) throws InterruptedException {
-        handleDelay();
+        try (Scope scope = span.makeCurrent()) {
+            span.setAttribute("db.system", "redis");
+            span.setAttribute("db.connection_string", redisHost);
+            span.setAttribute("db.statement", "GET user_id" + user_id);
 
-        logger.info("Getting favorites for user " + user_id);
+            handleDelay();
 
-        List<String> favorites = new ArrayList<>(r.getResource().smembers(user_id));
-        JSONObject favorites_json = new JSONObject();
-        favorites_json.put("favorites", favorites);
+            logger.info("Getting favorites for user " + user_id);
 
-        logger.info("User " + user_id + " has favorites " + favorites);
+            List<String> favorites = new ArrayList<>(jedis.smembers(user_id));
+            JSONObject favorites_json = new JSONObject();
+            favorites_json.put("favorites", favorites);
 
-        return favorites_json.toString();
+            logger.info("User " + user_id + " has favorites " + favorites);
+
+            returnedFavorites =  favorites_json.toString();
+
+        } catch (Exception e) {
+            span.setStatus(StatusCode.ERROR, "Error while getting data from Redis");
+            span.recordException(e);
+        } finally {
+            jedis.close();
+            span.end();
+        }
+        return returnedFavorites;
     }
+
+
 
     private void handleDelay() throws InterruptedException {
         if (delayTime > 0) {
@@ -127,7 +172,7 @@ public class ApiServlet {
     }
 
     private void handleCanary() throws Exception {
-        Span span = GlobalOpenTelemetry.getTracer("YEAH").spanBuilder("handleCanary").startSpan();
+        Span span = GlobalOpenTelemetry.getTracer(SERVICE_NAME).spanBuilder("handleCanary").startSpan();
         Scope scope = span.makeCurrent();
 
         Integer sleepTime =
--- src/main/java/com/movieapi/FavoriteApplication.java
+++ src/main/java/com/movieapi/FavoriteApplication.java
@@ -1,42 +1,27 @@
 package com.movieapi;
 
-import io.opentelemetry.api.metrics.Meter;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.logs.GlobalLoggerProvider;
 import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
 import io.opentelemetry.context.propagation.ContextPropagators;
-import io.opentelemetry.exporter.otlp.http.logs.OtlpHttpLogRecordExporter;
-import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
 import io.opentelemetry.exporter.otlp.logs.OtlpGrpcLogRecordExporter;
-import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor;
-import jakarta.servlet.ServletContextEvent;
-import jakarta.servlet.ServletContextListener;
-import io.opentelemetry.api.GlobalOpenTelemetry;
-import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
-import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.trace.SdkTracerProvider;
-import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
-import io.opentelemetry.sdk.trace.export.SpanExporter;
+import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.logs.SdkLoggerProvider;
-import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
-
-import io.opentelemetry.sdk.resources.Resource;
-import io.opentelemetry.sdk.trace.SdkTracerProvider;
-import io.opentelemetry.sdk.trace.samplers.Sampler;
+import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor;
 import io.opentelemetry.sdk.metrics.SdkMeterProvider;
 import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
-import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import io.opentelemetry.sdk.trace.samplers.Sampler;
 import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
-import io.opentelemetry.api.common.AttributeKey;
-
-import jakarta.servlet.annotation.WebListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FavoriteApplication {
@@ -48,7 +33,7 @@ public class FavoriteApplication {
 
 		// set service name on all OTel signals
 		Resource resource = Resource.getDefault().merge(Resource.create(
-				Attributes.of(ResourceAttributes.SERVICE_NAME, SERVICE_NAME)));
+				Attributes.of(ResourceAttributes.SERVICE_NAME, SERVICE_NAME,ResourceAttributes.SERVICE_VERSION,"1.0",ResourceAttributes.DEPLOYMENT_ENVIRONMENT,"production")));
 
 		// init OTel logger provider with export to OTLP
 		SdkLoggerProvider sdkLoggerProvider = SdkLoggerProvider.builder()
